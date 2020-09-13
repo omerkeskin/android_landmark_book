6cc859877067f71056cd7acbfb00404b9e97c7ba
Ömer Keskin <omer.keskin@softtech.com.tr>
2020-09-14 00:07:40 +0300

    Array adapter was created

--- app/src/main/java/com/omerkeskin/landmarkbook/MainActivity.java
+++ app/src/main/java/com/omerkeskin/landmarkbook/MainActivity.java
@@ -3,12 +3,28 @@ package com.omerkeskin.landmarkbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvLandmark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> landmarkNames = new ArrayList<>();
+        landmarkNames.add("Pisa");
+        landmarkNames.add("Eiffel");
+        landmarkNames.add("Colleseo");
+        landmarkNames.add("London Bridge");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, landmarkNames);
+        lvLandmark = findViewById(R.id.lvLandmark);
+        lvLandmark.setAdapter(adapter);
     }
 }
